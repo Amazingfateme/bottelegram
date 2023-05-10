ef2b9bbd325953129738c53cdcbf766713da159e
fateme hashemi <fateme.7575h@gmail.com>
2023-05-10 13:44:42 +0330

    bottlgrm

---
+++ MyBot/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace MyBot
+{
+    partial class frmmain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.GroupBox groupBox1;
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgReport = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnPhoto = new System.Windows.Forms.Button();
+            this.btnVideo = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtChannel = new System.Windows.Forms.TextBox();
+            this.btnSendText = new System.Windows.Forms.Button();
+            this.btnSendPhoto = new System.Windows.Forms.Button();
+            this.btnSendVideo = new System.Windows.Forms.Button();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReport)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(this.btnStart);
+            groupBox1.Controls.Add(this.txtToken);
+            groupBox1.Location = new System.Drawing.Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            groupBox1.Size = new System.Drawing.Size(625, 66);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Token";
+            groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(23, 22);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(106, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(135, 22);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtToken.Size = new System.Drawing.Size(478, 23);
+            this.txtToken.TabIndex = 0;
+            this.txtToken.Text = "5632169095:AAHxtvAssurR_BKk7CwFFTWnBmDslHcsqAk";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 591);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(645, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(54, 20);
+            this.lblStatus.Text = "Offline";
+            // 
+            // dgReport
+            // 
+            this.dgReport.AllowUserToAddRows = false;
+            this.dgReport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgReport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgReport.Location = new System.Drawing.Point(12, 84);
+            this.dgReport.Name = "dgReport";
+            this.dgReport.ReadOnly = true;
+            this.dgReport.RowHeadersWidth = 51;
+            this.dgReport.RowTemplate.Height = 24;
+            this.dgReport.Size = new System.Drawing.Size(625, 277);
+            this.dgReport.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "chatId";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "UserName";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Command";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "MessageID";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Date";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(89, 368);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(548, 88);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 368);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 88);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(218, 462);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(419, 23);
+            this.txtFilePath.TabIndex = 7;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(174, 462);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(38, 23);
+            this.btnSelectFile.TabIndex = 8;
+            this.btnSelectFile.Text = "...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnPhoto
+            // 
+            this.btnPhoto.Location = new System.Drawing.Point(93, 462);
+            this.btnPhoto.Name = "btnPhoto";
+            this.btnPhoto.Size = new System.Drawing.Size(75, 23);
+            this.btnPhoto.TabIndex = 9;
+            this.btnPhoto.Text = "Photo";
+            this.btnPhoto.UseVisualStyleBackColor = true;
+            this.btnPhoto.Click += new System.EventHandler(this.btnPhoto_Click);
+            // 
+            // btnVideo
+            // 
+            this.btnVideo.Location = new System.Drawing.Point(12, 462);
+            this.btnVideo.Name = "btnVideo";
+            this.btnVideo.Size = new System.Drawing.Size(75, 23);
+            this.btnVideo.TabIndex = 10;
+            this.btnVideo.Text = "Video";
+            this.btnVideo.UseVisualStyleBackColor = true;
+            this.btnVideo.Click += new System.EventHandler(this.btnVideo_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSendVideo);
+            this.groupBox2.Controls.Add(this.btnSendPhoto);
+            this.groupBox2.Controls.Add(this.btnSendText);
+            this.groupBox2.Controls.Add(this.txtChannel);
+            this.groupBox2.Location = new System.Drawing.Point(0, 491);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(637, 80);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "کانال";
+            // 
+            // txtChannel
+            // 
+            this.txtChannel.Location = new System.Drawing.Point(401, 36);
+            this.txtChannel.Name = "txtChannel";
+            this.txtChannel.Size = new System.Drawing.Size(230, 23);
+            this.txtChannel.TabIndex = 0;
+            // 
+            // btnSendText
+            // 
+            this.btnSendText.Location = new System.Drawing.Point(308, 36);
+            this.btnSendText.Name = "btnSendText";
+            this.btnSendText.Size = new System.Drawing.Size(87, 23);
+            this.btnSendText.TabIndex = 1;
+            this.btnSendText.Text = "Send Text";
+            this.btnSendText.UseVisualStyleBackColor = true;
+            this.btnSendText.Click += new System.EventHandler(this.btnSendText_Click);
+            // 
+            // btnSendPhoto
+            // 
+            this.btnSendPhoto.Location = new System.Drawing.Point(206, 36);
+            this.btnSendPhoto.Name = "btnSendPhoto";
+            this.btnSendPhoto.Size = new System.Drawing.Size(96, 23);
+            this.btnSendPhoto.TabIndex = 2;
+            this.btnSendPhoto.Text = "Send Photo";
+            this.btnSendPhoto.UseVisualStyleBackColor = true;
+            this.btnSendPhoto.Click += new System.EventHandler(this.btnSendPhoto_Click);
+            // 
+            // btnSendVideo
+            // 
+            this.btnSendVideo.Location = new System.Drawing.Point(111, 36);
+            this.btnSendVideo.Name = "btnSendVideo";
+            this.btnSendVideo.Size = new System.Drawing.Size(89, 23);
+            this.btnSendVideo.TabIndex = 3;
+            this.btnSendVideo.Text = "Send Video";
+            this.btnSendVideo.UseVisualStyleBackColor = true;
+            this.btnSendVideo.Click += new System.EventHandler(this.btnSendVideo_Click);
+            // 
+            // frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 617);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnVideo);
+            this.Controls.Add(this.btnPhoto);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.dgReport);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmmain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmmain_FormClosing);
+            this.Load += new System.EventHandler(this.frmmain_Load);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReport)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.DataGridView dgReport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnPhoto;
+        private System.Windows.Forms.Button btnVideo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSendVideo;
+        private System.Windows.Forms.Button btnSendPhoto;
+        private System.Windows.Forms.Button btnSendText;
+        private System.Windows.Forms.TextBox txtChannel;
+    }
+}
+
---
+++ MyBot/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+using Telegram.Bot.Requests;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace MyBot
+{
+    public partial class frmmain : Form
+    {
+        private static string Token = "";
+        private Thread botThread;
+        private Telegram.Bot.TelegramBotClient bot;
+        private ReplyKeyboardMarkup mainKeyboardMarkup;
+        private int bale1, bale2, bale3 = 0;
+        public string res1 = "1";
+        public string res2 = "2";
+        public string res3 = "3";
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Token = txtToken.Text;
+            botThread = new Thread(new ThreadStart(runBot));
+            botThread.Start();
+        }
+
+        private void frmmain_Load(object sender, EventArgs e)
+        {
+
+            KeyboardButton[] row1 =
+               {
+                new KeyboardButton("درباره ما"+"\U00002764"),
+                new KeyboardButton("تماس با ما"+"\U00002709")
+
+                };
+            KeyboardButton[] row2 =
+                {
+                   new KeyboardButton("آدرس ما"+ " \U0001F68C"),new KeyboardButton("نظر سنجی"+"\U0001F6A5")
+                };
+            mainKeyboardMarkup = new ReplyKeyboardMarkup(row1);
+            mainKeyboardMarkup.Keyboard = new KeyboardButton[][]
+                {
+                row1,row2
+                };
+
+
+
+        }
+
+        void runBot()
+        {
+            bot = new Telegram.Bot.TelegramBotClient(Token);
+
+            this.Invoke(new Action(() =>
+            {
+
+                lblStatus.Text = "Online";
+                lblStatus.ForeColor = Color.Green;
+
+            }));
+
+            int offset = 0;
+
+            while (true)
+            {
+
+                Telegram.Bot.Types.Update[] update = bot.GetUpdatesAsync(offset).Result;
+             
+
+                foreach (var up in update)
+                {
+                    offset = up.Id + 1;
+
+                    if (up.CallbackQuery != null)
+                    {
+                        switch (up.CallbackQuery.Data)
+                        {
+                            case "1":
+                                {
+                                bale1 +=1;
+                                break;
+                                }
+                            case "2":
+                                {
+                                    bale2 +=1;
+                                    break;
+                                }
+                            case "3":
+                                {
+                                    bale3 +=1;
+                                    break;
+                                }                      
+                        }
+                        InlineKeyboardButton[] row1 =
+                           {
+                            InlineKeyboardButton.WithCallbackData("چرا چرا چرااا(" + bale1 + ")", res1)
+                            ,InlineKeyboardButton.WithCallbackData(" چرا چرااا(" + bale2 + ")", res2)
+                            ,InlineKeyboardButton.WithCallbackData(" چرا بله(" + bale3 + ")", res2)
+                            };
+                        InlineKeyboardMarkup inline = new InlineKeyboardMarkup(row1);
+
+                        bot.EditMessageReplyMarkupAsync(up.CallbackQuery.Message.Chat.Id, up.CallbackQuery.Message.MessageId, inline, default);
+                    }
+                    
+                    if (up.Message == null)
+                        continue;
+
+                    var text = up.Message.Text.ToLower();
+                    var from = up.Message.From;
+                    var chatId = up.Message.Chat.Id;
+
+                    if (text.Contains("/start"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("به بات ما خوش امدید");
+                        sb.Append("میتوانید از امکاناتی که در اختیار شما قرار داده ایم استفاده کنید");
+                        sb.AppendLine("درباره ما:/aboutus");
+                        sb.AppendLine("تماس با ما:/ContactUs");
+                        sb.AppendLine("آدرس ما:/Address");
+
+                        bot.SendTextMessageAsync(chatId, sb.ToString(), default, null, null, null, null, null, null, mainKeyboardMarkup, default);
+                    }
+                    else if (text.Contains("/AboutUs") || text.Contains("درباره ما"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("ما خیلی خوبیم ");
+                        bot.SendTextMessageAsync(chatId, sb.ToString());
+                    }
+                    else if (text.Contains("/ContactUs") || text.Contains("تماس با ما"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("email:fateme.fornewwork@gmail.com");
+                        sb.AppendLine("mobile:09210127574");
+
+                        KeyboardButton[] row1 =
+                            {
+                             new KeyboardButton("تماس با علی صفری"),new KeyboardButton("تماس با فاطمه جون "),new KeyboardButton("تماس با بقیه ادمین ها")
+                            };
+                        KeyboardButton[] row2 =
+                            {
+                             new KeyboardButton("بازگشت")
+                            };
+                        ReplyKeyboardMarkup contactKeyboardMarkup = new ReplyKeyboardMarkup(row1);
+                        contactKeyboardMarkup.Keyboard = new KeyboardButton[][]
+                        {
+                            row1,row2
+                        };
+
+
+                        bot.SendTextMessageAsync(chatId, sb.ToString(), default, null, null, null, null, null, null, contactKeyboardMarkup, default);
+                    }
+                    else if (text.Contains("/Address") || text.Contains("آدرس ما"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("www.WhaleTrapGroup.com");
+
+
+                        InlineKeyboardButton[] row1 =
+                            {
+                              InlineKeyboardButton.WithUrl("varzesh3","http://varzesh3.com"),
+                               InlineKeyboardButton.WithUrl("tgju","http://tgju.org")
+
+                            };
+                        InlineKeyboardMarkup inline = new InlineKeyboardMarkup(row1);                        
+
+
+                        bot.SendTextMessageAsync(chatId, sb.ToString(), null, null, null, null, null, null, null, inline, default);
+                    }
+                    else if (text.Contains("بازگشت"))
+                    {
+                        bot.SendTextMessageAsync(chatId, "بازگشت به منوی اصلی", null, null, null, null, null, null, null, mainKeyboardMarkup, default);
+                    }
+                    else if (text.Contains("نظر سنجی"))
+                    {
+                        InlineKeyboardButton[] row1 =
+                            {
+                            InlineKeyboardButton.WithCallbackData("چرا چرا چرااا(" + bale1 + ")", res1)
+                            ,InlineKeyboardButton.WithCallbackData(" چرا چرااا(" + bale2 + ")", res2)
+                            ,InlineKeyboardButton.WithCallbackData(" چرا بله(" + bale3 + ")", res2)
+                            };                      
+                        InlineKeyboardMarkup inline = new InlineKeyboardMarkup(row1);
+                        //inline.InlineKeyboard = new InlineKeyboardButton[][]
+                        //{
+                        //row1,row2,row3
+                        //};
+                        bot.SendTextMessageAsync(chatId, "آیا از  ربات فاطمه راضی هستید؟",null,null,null,null,null,null,null,inline,default);
+
+                    }
+
+
+
+
+
+                    dgReport.Invoke(new Action(() =>
+                    {
+                        dgReport.Rows.Add(chatId, from.Username, text, up.Message.MessageId,
+                            up.Message.Date.ToString("yyyy/MM/dd - HH:mm"));
+
+                    }));
+                }
+            }
+        }
+
+        private void frmmain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            botThread.Abort();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (dgReport.CurrentRow != null)
+            {
+                int ChatID = int.Parse(dgReport.CurrentRow.Cells[0].Value.ToString());
+                bot.SendTextMessageAsync(ChatID, txtMessage.Text,ParseMode.Html,null, true,null,null,null,null,null,default);
+                txtMessage.Text = "";
+            }
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFile.FileName;
+            }
+        }
+
+        private void btnPhoto_Click(object sender, EventArgs e)
+        {
+            if (dgReport.CurrentRow != null)
+            {
+                int ChatID = int.Parse(dgReport.CurrentRow.Cells[0].Value.ToString());
+                FileStream imagefile = System.IO.File.Open(txtFilePath.Text, FileMode.Open);
+              
+                bot.SendPhotoAsync(ChatID,imagefile,txtMessage.Text,null,null,null,null,null,null,null,default);
+
+            }
+        }
+
+        private void btnVideo_Click(object sender, EventArgs e)
+        {
+            if (dgReport.CurrentRow != null)
+            {
+                int ChatID = int.Parse(dgReport.CurrentRow.Cells[0].Value.ToString());
+                FileStream videofile = System.IO.File.Open(txtFilePath.Text, FileMode.Open);
+                bot.SendVideoAsync(ChatID,videofile,null,null,null,null,null,null,null,null,null,null,null,null,null,default);
+
+            }
+        }
+
+        private void btnSendText_Click(object sender, EventArgs e)
+        {
+            bot.SendTextMessageAsync(txtChannel.Text, txtMessage.Text, ParseMode.Html);
+        }
+
+        private void btnSendPhoto_Click(object sender, EventArgs e)
+        {
+            FileStream imagefile = System.IO.File.Open(txtFilePath.Text, FileMode.Open);
+            bot.SendPhotoAsync(txtChannel.Text,imagefile,txtMessage.Text,null,null,null,null,null,null,null,default);
+        }
+
+        private void btnSendVideo_Click(object sender, EventArgs e)
+        {
+            FileStream videofile = System.IO.File.Open(txtFilePath.Text, FileMode.Open);
+            bot.SendVideoAsync(txtChannel.Text,videofile, null, null, null, null, null, null, null, default);
+        }
+    }
+}
